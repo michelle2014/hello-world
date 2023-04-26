f275af2104c75663efc6bbc5d572281012e2efa6
Michelle <michelle.transbetter@gmail.com>
2023-04-26 17:56:04 +1000

    attempt to fix environment variable problem

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 cd /var/www/html/
 password=$(aws ssm get-parameters --region us-west-2 --names Environment_Variables --with-decryption --query Parameters[0].Value)
+# get ride of "
 password=`echo $password | sed -e 's/^"//' -e 's/"$//'`
+# separate variables with $
 IFS='&' read -r  DB_HOST DB_PORT DB_SCHEMA DB_DATABASE DB_USERNAME DB_PASSWORD DB_ROOT_PASSWORD <<< $password
 
-sudo bash -c "echo $DB_HOST> .env"
-sudo bash -c "echo $DB_PORT> .env"
-sudo bash -c "echo $DB_SCHEMA> .env"
-sudo bash -c "echo $DB_DATABASE> .env"
-sudo bash -c "echo $DB_USERNAME> .env"
-sudo bash -c "echo $DB_PASSWORD> .env"
-sudo bash -c "echo $DB_ROOT_PASSWORD> .env"
-# echo $DB_HOST >> ".env"
-# echo $DB_PORT >> ".env"
-# echo $DB_SCHEMA >> ".env"
-# echo $DB_DATABASE >> ".env"
-# echo $DB_USERNAME >> ".env"
-# echo $DB_PASSWORD >> ".env"
-# echo $DB_ROOT_PASSWORD >> ".env"
+# add environment variables to .env file
+sudo bash -c "echo $DB_HOST>> .env"
+sudo bash -c "echo $DB_PORT>> .env"
+sudo bash -c "echo $DB_SCHEMA>> .env"
+sudo bash -c "echo $DB_DATABASE>> .env"
+sudo bash -c "echo $DB_USERNAME>> .env"
+sudo bash -c "echo $DB_PASSWORD>> .env"
+sudo bash -c "echo $DB_ROOT_PASSWORD>> .env"
